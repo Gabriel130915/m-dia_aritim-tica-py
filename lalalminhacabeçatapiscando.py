8ef3f53a28fbc31853772d7d1ecfaaf3416da281
Gabriel130915 <107150658+Gabriel130915@users.noreply.github.com>
2023-05-12 22:35:08 -0300

    Add files via upload

---
+++ lalalminhacabeçatapiscando.py
@@ -0,0 +1,13 @@
+from array import *
+
+def formula():
+    soma = 0
+    sla =  [24, 25, 26, 27, 28]
+    for x in sla:
+        soma = soma+x
+    print(soma)
+    print(soma/len(sla))
+
+
+
+formula()
